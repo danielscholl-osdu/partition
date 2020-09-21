f89ba54a0be1993bd57e0c087333d3675ebe3b48
Hong Yan <hyan6@slb.com>
2020-09-21 13:24:59 -0500

    merge from OSDU-delfi
    replace RedisCache with VmCache (loadingCache)
    
    # Conflicts:
    #	provider/partition-azure/src/main/resources/application.properties

--- provider/partition-azure/src/main/java/org/opengroup/osdu/partition/provider/azure/service/PartitionServiceCacheImpl.java
+++ provider/partition-azure/src/main/java/org/opengroup/osdu/partition/provider/azure/service/PartitionServiceCacheImpl.java
@@ -1,15 +1,13 @@
 package org.opengroup.osdu.partition.provider.azure.service;
 
-import org.opengroup.osdu.core.common.cache.RedisCache;
+import org.opengroup.osdu.core.common.cache.VmCache;
 import org.opengroup.osdu.partition.model.PartitionInfo;
 import org.opengroup.osdu.partition.provider.interfaces.IPartitionServiceCache;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PartitionServiceCacheImpl extends RedisCache<String, PartitionInfo> implements IPartitionServiceCache {
-    public PartitionServiceCacheImpl(@Value("${REDIS_HOST}") final String host
-            , @Value("${REDIS_PORT}") final int port) {
-        super(host, port, 60*60, String.class, PartitionInfo.class);
+public class PartitionServiceCacheImpl extends VmCache<String, PartitionInfo> implements IPartitionServiceCache {
+    public PartitionServiceCacheImpl() {
+        super(5*60, 1000);
     }
 }
