9932b0b6b2da96050c7045fbf94e081683681d53
Rucha Deshpande <deshruch@amazon.com>
2021-03-03 15:50:00 +0000

    Add List and Update Partition implementation and Int. tests
    commit 86bd8969 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Tue Mar 02 2021 16:58:34 GMT-0600 (Central Standard Time) 
    
        Bug fix: Partition is updated with field id
    
    
    commit bd100062 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Tue Mar 02 2021 15:37:36 GMT-0600 (Central Standard Time) 
    
        Fix imports
    
    
    commit 4a7f3ce9 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Tue Mar 02 2021 15:26:15 GMT-0600 (Central Standard Time) 
    
        fix return code
    
    
    commit d0f3b072 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Tue Mar 02 2021 15:23:10 GMT-0600 (Central Standard Time) 
    
        Add updatePartition and List all Partitions implementation
    
    
    commit 518affe3 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Tue Mar 02 2021 15:13:39 GMT-0600 (Central Standard Time) 
    
        Add updatePartition and List all Partitions implementation
    
    
    commit 98c0255e 
    Author: Rucha Deshpande <deshruch@amazon.com> 
    Date: Tue Mar 02 2021 10:58:41 GMT-0600 (Central Standard Time) 
    
        Add List and Update integration tests

---
+++ testing/partition-test-aws/src/test/java/org/opengroup/osdu/partition/api/TestListPartitions.java
@@ -0,0 +1,36 @@
+// Copyright © 2021 Amazon Web Services
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.opengroup.osdu.partition.api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.opengroup.osdu.partition.util.AwsTestUtils;
+
+
+public class TestListPartitions extends ListPartitionsApitTest {
+
+    @Before
+    @Override
+    public void setup() {
+        this.testUtils = new AwsTestUtils();
+    }
+
+    @After
+    @Override
+    public void tearDown() {
+        this.testUtils = null;
+    }
+
+}
