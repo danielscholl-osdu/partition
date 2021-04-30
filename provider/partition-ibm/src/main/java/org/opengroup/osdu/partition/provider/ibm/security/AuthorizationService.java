8811294780fb96b0dbd02089c4fea4119c5d0a42
Bhushan Rade <bhusrad1@in.ibm.com>
2021-04-30 08:52:30 +0000

    partitionid validation test case updated. no access token response code check changed 401 to 403

--- provider/partition-ibm/src/main/java/org/opengroup/osdu/partition/provider/ibm/security/AuthorizationService.java
+++ provider/partition-ibm/src/main/java/org/opengroup/osdu/partition/provider/ibm/security/AuthorizationService.java
@@ -3,47 +3,52 @@
 
 package org.opengroup.osdu.partition.provider.ibm.security;
 
-import org.opengroup.osdu.core.common.entitlements.IEntitlementsAndCacheService;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
 import org.opengroup.osdu.core.common.model.http.AppException;
-import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.opengroup.osdu.partition.provider.interfaces.IAuthorizationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @RequestScope
+@Slf4j
 public class AuthorizationService implements IAuthorizationService {
 
-    public static final String PARTITION_ADMIN_ROLE = "service.partition.admin";
+	@Value("${service.partition.admin.user}")
+	String partitionAdminUser;
 
-    @Autowired
-    private IEntitlementsAndCacheService entitlementsAndCacheService;
-    
-    @Autowired
-    private DpsHeaders headers;
 
-    @Override
-    public boolean isDomainAdminServiceAccount() {
-        try {
-            return hasRole(PARTITION_ADMIN_ROLE);
-        }
-        catch (AppException e) {
-            throw e;
-        }
-        catch (Exception e) {
-            throw new AppException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Authentication Failure", e.getMessage(), e);
-        }
-        
-    }
+	@Override
+	public boolean isDomainAdminServiceAccount() {
+		try {
+			final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			@SuppressWarnings("unchecked")
+			KeycloakPrincipal<KeycloakSecurityContext> principal = (KeycloakPrincipal<KeycloakSecurityContext>) auth.getPrincipal();
+			String upn =  principal.getName();
+			log.info("email : "+upn);
+			if(upn.equals(partitionAdminUser)) {
+				return true;
+			} 
+		    else {
+				throw AppException.createUnauthorized("Unauthorized. The user is not Service Principal");
+			}
+				
+		}
+		catch (AppException e) {
+			throw e;
+		}
+		catch (Exception e) {
+			throw new AppException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Authentication Failure", e.getMessage(), e);
+		}
 
-    private boolean hasRole(String requiredRole) {        
-        //headers.put(DpsHeaders.DATA_PARTITION_ID, PARTITION_NAME);
-        String user = entitlementsAndCacheService.authorize(headers, requiredRole);
-        headers.put(DpsHeaders.USER_EMAIL, user);
-        return true;
-    }
+	}
 
 }
 
