ad8f45ed8402d54cc7058e653f7cb7dd8dc02a53
Mingyang Zhu <mzhu9@slb.com>
2021-05-05 16:10:47 -0500

    add annotations

--- provider/partition-azure/src/main/java/org/opengroup/osdu/partition/provider/azure/service/HealthCheckServiceImpl.java
+++ provider/partition-azure/src/main/java/org/opengroup/osdu/partition/provider/azure/service/HealthCheckServiceImpl.java
@@ -1,11 +1,15 @@
 package org.opengroup.osdu.partition.provider.azure.service;
 
+import lombok.RequiredArgsConstructor;
 import org.opengroup.osdu.partition.model.PartitionInfo;
 import org.opengroup.osdu.partition.provider.interfaces.IHealthCheckService;
 import org.opengroup.osdu.partition.provider.interfaces.IPartitionServiceCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class HealthCheckServiceImpl implements IHealthCheckService {
 
     @Autowired
