9a2700fe97db79789bc4043819d21aef2c401f97
Mingyang Zhu <mzhu9@slb.com>
2021-05-05 16:10:47 -0500

    add annotations

--- provider/partition-gcp/src/main/java/org/opengroup/osdu/partition/provider/gcp/service/HealthCheckServiceImpl.java
+++ provider/partition-gcp/src/main/java/org/opengroup/osdu/partition/provider/gcp/service/HealthCheckServiceImpl.java
@@ -1,7 +1,9 @@
 package org.opengroup.osdu.partition.provider.gcp.service;
 
 import org.opengroup.osdu.partition.provider.interfaces.IHealthCheckService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HealthCheckServiceImpl implements IHealthCheckService {
 
     @Override
