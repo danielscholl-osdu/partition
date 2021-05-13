7593fb135b7c44e6668261cc2bb36072f9b34e66
Mingyang Zhu <mzhu9@slb.com>
2021-05-13 12:48:28 -0500

    fix the bean annotation

--- partition-core/src/main/java/org/opengroup/osdu/partition/service/DefaultHealthCheckImpl.java
+++ partition-core/src/main/java/org/opengroup/osdu/partition/service/DefaultHealthCheckImpl.java
@@ -1,11 +1,9 @@
 package org.opengroup.osdu.partition.service;
 
 import org.opengroup.osdu.partition.provider.interfaces.IHealthCheckService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 
 @Service
-@ConditionalOnMissingBean(value = IHealthCheckService.class)
 public class DefaultHealthCheckImpl implements IHealthCheckService {
 
     @Override
