44dc0b0018679f95f8c83ade8ea2040d241aed19
Mingyang Zhu <mzhu9@slb.com>
2021-05-18 10:21:06 -0500

    make azure custom readiness redis check optional

--- provider/partition-azure/src/main/java/org/opengroup/osdu/partition/provider/azure/service/HealthCheckServiceImpl.java
+++ provider/partition-azure/src/main/java/org/opengroup/osdu/partition/provider/azure/service/HealthCheckServiceImpl.java
@@ -2,11 +2,11 @@ package org.opengroup.osdu.partition.provider.azure.service;
 
 import lombok.RequiredArgsConstructor;
 import org.opengroup.osdu.partition.model.PartitionInfo;
-import org.opengroup.osdu.partition.provider.interfaces.IHealthCheckService;
 import org.opengroup.osdu.partition.provider.interfaces.IPartitionServiceCache;
 import org.opengroup.osdu.partition.service.DefaultHealthCheckImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +19,17 @@ public class HealthCheckServiceImpl extends DefaultHealthCheckImpl {
     @Qualifier("partitionServiceCache")
     private IPartitionServiceCache<String, PartitionInfo> partitionServiceCache;
 
+    @Value("${redis.custom.readiness.check.enabled}")
+    private boolean redisCustomReadinessCheck;
+
+
     /**
      * Cache layer must be ready before the pod can serve the traffic
      */
     @Override
     public void performReadinessCheck() {
-        partitionServiceCache.get("dummy-key");
+        if (redisCustomReadinessCheck) {
+            partitionServiceCache.get("dummy-key");
+        }
     }
 }
