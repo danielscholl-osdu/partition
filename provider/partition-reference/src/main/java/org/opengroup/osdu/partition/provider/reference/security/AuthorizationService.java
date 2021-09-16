c26651000253ca9c6ea054a2e756cd447d911a33
Riabokon Stanislav(EPAM) <stanislav_riabokon@epam.com>
2021-09-16 09:38:55 +0000

    Gcp anthos (GONRG-3336)

---
+++ provider/partition-reference/src/main/java/org/opengroup/osdu/partition/provider/reference/security/AuthorizationService.java
@@ -0,0 +1,93 @@
+/*
+  Copyright 2002-2021 Google LLC
+  Copyright 2002-2021 EPAM Systems, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package org.opengroup.osdu.partition.provider.reference.security;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.opengroup.osdu.core.common.model.http.AppException;
+import org.opengroup.osdu.core.common.model.http.DpsHeaders;
+import org.opengroup.osdu.partition.provider.interfaces.IAuthorizationService;
+import org.opengroup.osdu.partition.provider.reference.config.PropertiesConfiguration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Slf4j
+@Component
+@RequestScope
+@RequiredArgsConstructor
+public class AuthorizationService implements IAuthorizationService {
+
+  private final PropertiesConfiguration configuration;
+
+  private final DpsHeaders headers;
+
+  private final GoogleIdTokenVerifier verifier;
+
+  @Override
+  public boolean isDomainAdminServiceAccount() {
+    if (Objects.isNull(headers.getAuthorization()) || headers.getAuthorization().isEmpty()) {
+      throw AppException.createUnauthorized("No JWT token. Access is Forbidden");
+    }
+    String email = null;
+    try {
+      String authorization = headers.getAuthorization().replace("Bearer ", "");
+      GoogleIdToken googleIdToken = verifier.verify(authorization);
+      if (Objects.isNull(googleIdToken)) {
+        log.warn("Not valid token provided");
+        throw AppException.createUnauthorized("Unauthorized. The JWT token could not be validated");
+      }
+      email = googleIdToken.getPayload().getEmail();
+      List<String> partitionAdminAccounts = configuration.getPartitionAdminAccounts();
+      if (Objects.nonNull(partitionAdminAccounts) && !partitionAdminAccounts.isEmpty()) {
+        return isAllowedAccount(email);
+      } else {
+        if (StringUtils.endsWith(email, configuration.getServiceAccountTail())) {
+          return true;
+        } else {
+          throw AppException.createUnauthorized(
+              String.format("Unauthorized. The user %s is not Service Principal", email));
+        }
+      }
+    } catch (AppException e){
+      throw e;
+    } catch (Exception ex) {
+      log.warn(String.format("User %s is not unauthorized. %s.", email, ex));
+      throw AppException.createUnauthorized("Unauthorized. The JWT token could not be validated");
+    }
+  }
+
+  private boolean isAllowedAccount(String accountEmail) {
+    if (StringUtils.endsWith(accountEmail, configuration.getServiceAccountTail())) {
+      for (String partitionAdmin : configuration.getPartitionAdminAccounts()) {
+        if (partitionAdmin.equals(accountEmail)) {
+          return true;
+        }
+        if (StringUtils.startsWith(accountEmail, partitionAdmin)) {
+          return true;
+        }
+      }
+    }
+    throw AppException
+        .createUnauthorized(String.format("Unauthorized. The user %s is untrusted.", accountEmail));
+  }
+}
---
+++ provider/partition-reference/src/main/java/org/opengroup/osdu/partition/provider/reference/service/PartitionServiceImpl.java
@@ -0,0 +1,124 @@
+/*
+  Copyright 2002-2021 Google LLC
+  Copyright 2002-2021 EPAM Systems, Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package org.opengroup.osdu.partition.provider.reference.service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.opengroup.osdu.core.common.model.http.AppException;
+import org.opengroup.osdu.core.common.provider.interfaces.IKmsClient;
+import org.opengroup.osdu.partition.logging.AuditLogger;
+import org.opengroup.osdu.partition.model.PartitionInfo;
+import org.opengroup.osdu.partition.model.Property;
+import org.opengroup.osdu.partition.provider.interfaces.IPartitionService;
+import org.opengroup.osdu.partition.provider.reference.repository.PartitionPropertyEntityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PartitionServiceImpl implements IPartitionService {
+
+  private static final String UNKNOWN_ERROR_REASON = "unknown error";
+
+  private final PartitionPropertyEntityRepository partitionPropertyEntityRepository;
+
+  private final IKmsClient kmsClient;
+
+  private final AuditLogger auditLogger;
+
+  @Override
+  public PartitionInfo createPartition(String partitionId, PartitionInfo partitionInfo) {
+    if (partitionPropertyEntityRepository.findByPartitionId(partitionId).isPresent()) {
+      throw new AppException(HttpStatus.SC_CONFLICT, UNKNOWN_ERROR_REASON,
+          "Partition already exists.");
+    }
+    partitionInfo.getProperties()
+        .forEach((key, property) -> encryptPartitionPropertyEntityIfNeeded(property));
+    partitionPropertyEntityRepository.createPartition(partitionId, partitionInfo);
+    return getPartition(partitionId);
+  }
+
+  @Override
+  public PartitionInfo updatePartition(String partitionId, PartitionInfo partitionInfo) {
+    if (partitionInfo.getProperties().containsKey("id")) {
+      this.auditLogger.updatePartitionSecretFailure(Collections.singletonList(partitionId));
+      throw new AppException(HttpStatus.SC_BAD_REQUEST, "can not update id",
+          "the field id can not be updated");
+    }
+    if (!partitionPropertyEntityRepository.findByPartitionId(partitionId).isPresent()) {
+      this.auditLogger.updatePartitionSecretFailure(Collections.singletonList(partitionId));
+      throw new AppException(HttpStatus.SC_NOT_FOUND, UNKNOWN_ERROR_REASON,
+          "An attempt to update not existing partition.");
+    }
+    partitionInfo.getProperties().forEach((key, value) -> {
+      Optional<Property> property = this.partitionPropertyEntityRepository
+          .findByPartitionIdAndName(partitionId, key);
+      if (property.isPresent()) {
+        property.get().setSensitive(value.isSensitive());
+        property.get().setValue(value.getValue());
+        encryptPartitionPropertyEntityIfNeeded(property.get());
+      }
+      encryptPartitionPropertyEntityIfNeeded(value);
+    });
+    partitionPropertyEntityRepository.updatePartition(partitionId, partitionInfo);
+    return getPartition(partitionId);
+  }
+
+  @Override
+  public PartitionInfo getPartition(String partitionId) {
+    Optional<PartitionInfo> result = partitionPropertyEntityRepository.findByPartitionId(
+        partitionId);
+    if (result.isPresent()) {
+      result.get().getProperties()
+          .forEach((key, property) -> decryptPartitionPropertyIfNeeded(property));
+      return result.get();
+    }
+    return new PartitionInfo();
+  }
+
+  @Override
+  public boolean deletePartition(String partitionId) {
+    if (!partitionPropertyEntityRepository.findByPartitionId(partitionId).isPresent()) {
+      this.auditLogger.deletePartitionFailure(Collections.singletonList(partitionId));
+      throw new AppException(HttpStatus.SC_NOT_FOUND, UNKNOWN_ERROR_REASON,
+          "An attempt to delete not existing partition.");
+    }
+    return partitionPropertyEntityRepository.isDeletedPartitionInfoByPartitionId(partitionId);
+  }
+
+  @Override
+  public List<String> getAllPartitions() {
+    return partitionPropertyEntityRepository.getAllPartitions();
+  }
+
+  private void encryptPartitionPropertyEntityIfNeeded(Property property) {
+    if (property.isSensitive()) {
+      String propertyValue = property.getValue().toString();
+      property.setValue("this.kmsClient.encryptString " + propertyValue);
+    }
+  }
+
+  private void decryptPartitionPropertyIfNeeded(Property property) {
+    if (property.isSensitive()) {
+      String propertyValue = property.getValue().toString();
+      property.setValue("this.kmsClient.decryptString " + propertyValue);
+    }
+  }
+}
