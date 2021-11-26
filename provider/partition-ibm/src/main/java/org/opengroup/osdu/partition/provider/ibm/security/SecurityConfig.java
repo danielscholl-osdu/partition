b40956f1ac8b73f9c929c3c39607b78bde23f86f
BhushanRade <bhusrad1@in.ibm.com>
2021-11-26 12:54:27 +0530

    disable spring security for ibm

--- provider/partition-ibm/src/main/java/org/opengroup/osdu/partition/provider/ibm/security/SecurityConfig.java
+++ provider/partition-ibm/src/main/java/org/opengroup/osdu/partition/provider/ibm/security/SecurityConfig.java
@@ -15,8 +15,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable()
-                .csrf().disable().authorizeRequests().anyRequest()
-                .authenticated().and().oauth2ResourceServer().jwt();  
+                .csrf().disable();
+                //.authorizeRequests().anyRequest()
+                //.authenticated().and().oauth2ResourceServer().jwt();  
     }
 }
 
