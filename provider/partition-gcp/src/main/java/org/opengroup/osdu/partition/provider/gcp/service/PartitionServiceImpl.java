3511595bb0e74b4390c0d0b4bd72f3192c55ba71
Artem Dobrynin (EPAM) <artem_dobrynin@epam.com>
2021-12-29 14:13:37 +0000

    Migrate Partition Service to Anthos (new approach) (GONRG-3881)

--- provider/partition-gcp/src/main/java/org/opengroup/osdu/partition/provider/gcp/service/PartitionServiceImpl.java
+++ provider/partition-gcp/src/main/java/org/opengroup/osdu/partition/provider/gcp/service/PartitionServiceImpl.java
@@ -1,18 +1,18 @@
 /*
-  Copyright 2002-2021 Google LLC
-  Copyright 2002-2021 EPAM Systems, Inc
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+ * Copyright 2020-2021 Google LLC
+ * Copyright 2020-2021 EPAM Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.opengroup.osdu.partition.provider.gcp.service;
@@ -26,7 +26,7 @@ import org.opengroup.osdu.partition.logging.AuditLogger;
 import org.opengroup.osdu.partition.model.PartitionInfo;
 import org.opengroup.osdu.partition.model.Property;
 import org.opengroup.osdu.partition.provider.gcp.model.PartitionPropertyEntity;
-import org.opengroup.osdu.partition.provider.gcp.repository.PartitionPropertyEntityRepository;
+import org.opengroup.osdu.partition.provider.gcp.osm.repository.OsmPartitionPropertyRepository;
 import org.opengroup.osdu.partition.provider.interfaces.IPartitionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -47,7 +47,7 @@ public class PartitionServiceImpl implements IPartitionService {
   static final String UNKNOWN_ERROR_REASON = "unknown error";
   static final String PARTITION_LIST_KEY = "getAllPartitions";
 
-  private final PartitionPropertyEntityRepository partitionPropertyEntityRepository;
+  private final OsmPartitionPropertyRepository partitionPropertyEntityRepository;
 
   private final IKmsClient kmsClient;
 
@@ -74,10 +74,7 @@ public class PartitionServiceImpl implements IPartitionService {
       encryptPartitionPropertyEntityIfNeeded(entity);
       partitionProperties.add(entity);
     }
-    this.partitionPropertyEntityRepository.performTransaction(repository -> {
-      repository.saveAll(partitionProperties);
-      return true;
-    });
+    this.partitionPropertyEntityRepository.saveAll(partitionProperties);
     PartitionInfo pi = getPartition(partitionId);
 
     if (pi != null) {
@@ -126,10 +123,7 @@ public class PartitionServiceImpl implements IPartitionService {
       encryptPartitionPropertyEntityIfNeeded(entity);
       partitionProperties.add(entity);
     }
-    this.partitionPropertyEntityRepository.performTransaction(repository -> {
-      repository.saveAll(partitionProperties);
-      return true;
-    });
+    this.partitionPropertyEntityRepository.saveAll(partitionProperties);
 
     return getPartition(partitionId);
   }
