e74b91fe329af7e7c28955c100c5a437f2fb38a6
Deepa_Kumari <deepa_kumari@epam.com>
2023-09-20 13:51:30 +0530

    modify properties and add tests

--- partition-core-plus/src/main/java/org/opengroup/osdu/partition/coreplus/PartitionApplication.java
+++ partition-core-plus/src/main/java/org/opengroup/osdu/partition/coreplus/PartitionApplication.java
@@ -14,12 +14,18 @@
 
 package org.opengroup.osdu.partition.coreplus;
 
+import org.opengroup.osdu.core.osm.postgresql.PgTenantOsmDestinationResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan({"org.opengroup.osdu"})
+@ComponentScan(basePackages = {"org.opengroup.osdu"}, excludeFilters =
+@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+        PgTenantOsmDestinationResolver.class
+}
+))
 @SpringBootApplication
 @PropertySource("classpath:swagger.properties")
 public class PartitionApplication {
