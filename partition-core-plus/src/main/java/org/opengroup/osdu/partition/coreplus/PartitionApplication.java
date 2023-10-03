4f7cf559ab95f0fa32742a626de6cd1bf5a042ac
Deepa_Kumari <deepa_kumari@epam.com>
2023-10-03 17:52:14 +0530

    adding core-plus implementation for partition service

---
+++ partition-core-plus/src/main/java/org/opengroup/osdu/partition/coreplus/PartitionApplication.java
@@ -0,0 +1,36 @@
+// Copyright 2017-2020, Schlumberger
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.opengroup.osdu.partition.coreplus;
+
+import org.opengroup.osdu.core.osm.postgresql.PgTenantOsmDestinationResolver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan(basePackages = {"org.opengroup.osdu"}, excludeFilters =
+@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+        PgTenantOsmDestinationResolver.class
+}
+))
+@SpringBootApplication
+@PropertySource("classpath:swagger.properties")
+public class PartitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartitionApplication.class, args);
+    }
+}
