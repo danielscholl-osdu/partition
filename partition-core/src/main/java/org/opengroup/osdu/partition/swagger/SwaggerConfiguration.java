4eb4ccbabbc47094d0e5fbf4c1c68dc536a0c903
Anthony Ittiyera Vazhappilly <avazhappilly@microsoft.com>
2024-02-09 11:54:48 -0800

    create a feature flag to toggle the fullUrl in swagger

--- partition-core/src/main/java/org/opengroup/osdu/partition/swagger/SwaggerConfiguration.java
+++ partition-core/src/main/java/org/opengroup/osdu/partition/swagger/SwaggerConfiguration.java
@@ -16,7 +16,7 @@ import javax.servlet.ServletContext;
 
 import io.swagger.v3.oas.models.tags.Tag;
 import org.opengroup.osdu.partition.model.Property;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -27,51 +27,38 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:swagger.properties")
 public class SwaggerConfiguration {
 
-    @Value("${api.title}")
-    private String apiTitle;
-
-    @Value("${api.description}")
-    private String apiDescription;
-
-    @Value("${api.version}")
-    private String apiVersion;
-
-    @Value("${api.contact.name}")
-    private String contactName;
-
-    @Value("${api.contact.email}")
-    private String contactEmail;
-
-    @Value("${api.license.name}")
-    private String licenseName;
-
-    @Value("${api.license.url}")
-    private String licenseUrl;
+    @Autowired
+    private SwaggerConfigurationProperties configurationProperties;
 
     @Bean
     public OpenAPI openApi(ServletContext servletContext) {
         Server server = new Server().url(servletContext.getContextPath());
-        return new OpenAPI()
-            .servers(Collections.singletonList(server))
-            .info(new Info()
-                .title(apiTitle)
-                .description(apiDescription)
-                .version(apiVersion)
-                .contact(new Contact().name(contactName).email(contactEmail))
-                .license(new License().name(licenseName).url(licenseUrl)))
-            .addTagsItem(new Tag().name("partition-api").description("Partition API"))
-            .addTagsItem(new Tag().name("info").description("Version info endpoint"))
-            .components(new Components()
-                    .addSchemas("Map", new Schema<Map<String, Property>>().addProperties("< * >", new ObjectSchema().$ref("#/components/schemas/Property")))
+        OpenAPI openAPI = new OpenAPI()
+                .info(new Info()
+                        .title(configurationProperties.getApiTitle())
+                        .description(configurationProperties.getApiDescription())
+                        .version(configurationProperties.getApiVersion())
+                        .contact(new Contact().name(configurationProperties.getApiContactName()).email(configurationProperties.getApiContactEmail()))
+                        .license(new License().name(configurationProperties.getApiLicenseName()).url(configurationProperties.getApiLicenseUrl())))
+                .addTagsItem(new Tag().name("partition-api").description("Partition API"))
+                .addTagsItem(new Tag().name("info").description("Version info endpoint"))
+                .components(new Components()
+                        .addSchemas("Map",
+                                new Schema<Map<String, Property>>().addProperty("< * >",
+                                        new ObjectSchema().$ref("#/components/schemas/Property")))
                         .addSecuritySchemes("Authorization",
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("Authorization")
-                        .in(SecurityScheme.In.HEADER)
-                        .name("Authorization")))
-            .addSecurityItem(
-                new SecurityRequirement()
-                    .addList("Authorization"));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("Authorization")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList("Authorization"));
+        if (configurationProperties.isApiServerFullUrlEnabled())
+            return openAPI;
+        return openAPI
+                .servers(Collections.singletonList(server));
     }
 }
